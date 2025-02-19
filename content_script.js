3d8449515ae1606e5a454709c665010f26fad581
Oleksandr Stelmashchuk <novus7827@gmail.com>
2025-02-19 13:06:04 +0200

    Added button to click on active marked icons on /selected/ page and hid veshka_container elements

--- content_script.js
+++ content_script.js
@@ -1,4 +1,22 @@
 function hideElementsByCondition() {
+    // Приховуємо роздільники
+    const delimiters = document.querySelectorAll('.requests_cards_delimiter');
+    delimiters.forEach(delimiter => {
+        delimiter.style.display = "none";
+    });
+
+    // Приховуємо елементи veshka_container
+    const veshkaContainers = document.querySelectorAll('.veshka_container');
+    veshkaContainers.forEach(container => {
+        container.style.display = "none";
+    });
+
+    // Перевіряємо, чи не знаходимось ми на сторінці /selected/
+    if (window.location.pathname.includes('/selected/')) {
+        addClickButton(); // Додаємо кнопку на сторінці /selected/
+        return;
+    }
+
     const blocks = document.querySelectorAll(".request_card");
     
     blocks.forEach(block => {
@@ -10,11 +28,36 @@ function hideElementsByCondition() {
             price  = paymentPriceSource.innerHTML.replace(/\D/g,'');            
         }
 
-      if (price < 4000) {
-        block.style.display = "none";
-
-      }
+        const markedIcon = block.querySelector('.marked_icon.is_active');
+        
+        if (price < 4500 || markedIcon !== null) {
+            block.style.display = "none";
+        }
     });
-  }
-  
-  window.addEventListener("load", hideElementsByCondition);
+}
+
+function addClickButton() {
+    const button = document.createElement('button');
+    button.innerText = 'Знайти всі заявки';
+    button.style.position = 'fixed';
+    button.style.left = '10px';
+    button.style.bottom = '10px';
+    button.style.zIndex = '1000';
+    button.style.padding = '10px';
+    button.style.backgroundColor = '#007BFF';
+    button.style.color = '#FFFFFF';
+    button.style.border = 'none';
+    button.style.borderRadius = '5px';
+    button.style.cursor = 'pointer';
+
+    button.addEventListener('click', () => {
+        const activeIcons = document.querySelectorAll('.marked_icon.is_active');
+        activeIcons.forEach(icon => {
+            icon.click(); // Клікаємо по кожному активному елементу
+        });
+    });
+
+    document.body.appendChild(button);
+}
+
+window.addEventListener("load", hideElementsByCondition);
